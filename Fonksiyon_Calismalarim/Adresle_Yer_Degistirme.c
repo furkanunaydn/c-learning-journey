7a6aec71436dea49fba4f5a766b206b44bae042f
furkanunaydin <furkanunaydn0@gmail.com>
2026-07-10 16:43:55 +0300

    Swap two numbers using pointers (by reference)
    - Fonksiyona x ve y'nin adreslerini gonderdim (&x, &y)
    - Fonksiyon icinde *x ve *y ile orijinal degiskenlere ulastim
    - Fonksiyon donusunde main'deki x ve y degisti
    - Pointer'in ana amacini kavradim

---
+++ Fonksiyon_Calismalarim/Adresle_Yer_Degistirme.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void yerDegistir(int *x, int *y)
+{
+    int gecici;
+
+    gecici = *x;
+    *x = *y;
+    *y = gecici;
+
+    printf("Yer degistirme islemi sonrasi: x = %d, y = %d\n", *x, *y);
+}
+int main()
+{
+
+    int x, y;
+
+    printf("2 deger giriniz: ");
+    scanf("%d%d", &x, &y);
+
+    printf("Yer degistirme islemi oncesi: x = %d, y = %d\n", x, y);
+    yerDegistir(&x, &y);
+
+    printf("Fonksiyondan sonra main'de: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
